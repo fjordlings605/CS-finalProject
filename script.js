5543369075c979850733ede7e13adf6392853382
Jon Ford <fjordlings605@gmail.com>
2023-06-09 22:57:26 -0500

    Added customizations - columns, dropboxes, background, etc

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ var toppings = [];
 var formBtn = document.getElementById("btn");
 
 formBtn.addEventListener("click",function(event){
-    event.preventDefault();
-
+    //event.preventDefault();
+    toppings = []; //reset toppings array to emtpy if button pressed  again
     var topping1 = document.getElementById("top1").value;
     var topping2 = document.getElementById("top2").value;
     var topping3 = document.getElementById("top3").value;
@@ -26,29 +26,31 @@ formBtn.addEventListener("click",function(event){
 
 
 function calculateTotal(toppingArray){
+    document.getElementById("total").innerHTML= "$";
     let total = 0;
-    let toppingCost = 2.50;
+    let toppingCost = 2.50; //cost per topping
     let baseCost = 5.50;   //cost of crust and sauce
 
     //order string concatenation
-    let orderString = crust + " Pizza with " + sauce;
-    let toppingString = "Toppings: ";
+    let orderString = crust + " Pizza with " + sauce + " Sauce - ";
+    let toppingString = "Toppings: &nbsp";
 
     //for loop
     for(let i=0; i < toppingArray.length; i++){
         if(toppingArray[i]!=""){
-            toppingString += toppingArray[i] + " ";
+            toppingString += "&nbsp" + toppingArray[i] + " ";
             total += toppingCost;
+            
         }
     }
-
+   
     // total = baseCost + cost of all toppings
     total += baseCost;
 
-    //set DOM total += total
-    document.getElementById("total").innerHTML += total;
+    //set DOM total += total & format to 2 decimal places
+    document.getElementById("total").innerHTML += total.toFixed(2);
     //use DOM: = orderString
     document.getElementById("pizzaOrder").innerHTML = orderString;
     //use DOM: = toppingString
     document.getElementById("toppings").innerHTML = toppingString;
-}
+} 
